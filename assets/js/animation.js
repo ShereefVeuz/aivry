32736a7e221cb2d4838498f8d35b4e11acd1bfff
shereef <shareef@veuz.in>
2025-07-21 19:22:09 +0530

    aivri twoone2 commit

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -12,16 +12,139 @@ gsap.registerPlugin(ScrollTrigger);
   //     start: "top 130%",
   //     end:"center 50%",
   //     scrub: true, 
-  //     markers: false,
+  //     markers: true,
   //     toggleActions: "play reverse play reverse",
   //   },
   // });
 
 
+// top-products
+gsap.from(".top-products .owl-carousel .item", {
+  duration: 1,       
+  x: 100,
+  opacity: 0,
+  stagger: 0.2,  
+  scrollTrigger: {
+    trigger: ".top-products",    
+    start: "top 80%",             
+    end: "bottom 30%",             
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+
+// top-categories
+gsap.from(".top-categories .owl-carousel .item", {
+  duration: 1,   
+  x: -100,
+  opacity: 0,
+  stagger: 0.2,   
+  scrollTrigger: {
+    trigger: ".top-categories",  
+    start: "top 80%",    
+    end: "bottom 30%",    
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+
+// top-products
+gsap.from(".most-freq-inner .owl-carousel .item", {
+  duration: 1,       
+  x: 100,
+  opacity: 0,
+  stagger: 0.2,  
+  scrollTrigger: {
+    trigger: ".most-freq-inner",    
+    start: "top 50%",             
+    end: "bottom 30%",             
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+
+// home-shiping-item
+document.addEventListener('DOMContentLoaded', function () {
+    gsap.registerPlugin(ScrollTrigger);
 
 
+gsap.from(".home-shiping-item", {
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    stagger: 0.3,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".home-shiping-item",
+        start: "top 60%",
+        toggleActions: "play none none reverse",
+    }
+});
 
+});
 
+// makup products
+gsap.from(".right-mkp-products-item", {
+  duration: 1,   
+  x: -100,
+  opacity: 0,
+  stagger: 0.2,   
+  scrollTrigger: {
+    trigger: ".right-mkp-products-item",  
+    start: "top 80%",    
+    end: "bottom 30%",    
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+gsap.from(".left-mkp-products-item", {
+  duration: 1,       
+  x: 100,
+  opacity: 0,
+  stagger: 0.2,  
+  scrollTrigger: {
+    trigger: ".left-mkp-products-item",    
+    start: "top 80%",             
+    end: "bottom 30%",             
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+
+// makup products
+gsap.from(".app-content-out", {
+  duration: 1,   
+  x: -100,
+  opacity: 0,
+  stagger: 0.2,   
+  scrollTrigger: {
+    trigger: ".app-content-out",  
+    start: "top 80%",    
+    end: "bottom 30%",    
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
+gsap.from(".testim-out", {
+  duration: 1,       
+  x: 100,
+  opacity: 0,
+  stagger: 0.2,  
+  scrollTrigger: {
+    trigger: ".testim-out",    
+    start: "top 80%",             
+    end: "bottom 30%",             
+    scrub: true,
+    markers: false,
+    toggleActions: "play none none reverse"
+  }
+});
 
 
 
